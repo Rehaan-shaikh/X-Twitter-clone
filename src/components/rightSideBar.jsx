8fa7ca3bd47439b29f5a435077bd4098195a0bea
Maaz-Shaikh-413 <shaikhmaaz8788@gmail.com>
2025-05-14 16:21:22 +0530

    side bar

--- src/components/rightSideBar.jsx
+++ src/components/rightSideBar.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+'use client' 
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react'
 
 const RightSideBar = () => {
+  const router = useRouter();;
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleInputChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if(searchTerm.trim()===0) return
+    router.push(`/search/${searchTerm}`);
+  };
+
   return (
     <div>
-      rightSideBar
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={handleInputChange}
+          placeholder="Search here"
+          className='bg-gray-700 border border-gray-200 rounded-3xl text-sm w-full px-4 py-2'
+        />
+      </form>
     </div>
-  )
+  );
 }
 
 export default RightSideBar
