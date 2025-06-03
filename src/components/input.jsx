3fa6de325c8f2a117c35baf96fc96fd1a83c603b
Maaz-Shaikh-413 <shaikhmaaz8788@gmail.com>
2025-06-03 14:11:49 +0530

    eeee

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,7 +1,7 @@
 'use server'
 
 import { getCurrentUser } from '@/lib/actions/user';
-import InputClient from './InputClient';
+import InputClient from './inputclient';
 
 export default async function Input() {
   const user = await getCurrentUser(); 
